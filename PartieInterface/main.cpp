ba5221223304a9481f57452acae88b06c73e2267
Kyllien <kyllien.romand@gmail.com>
2022-12-30 10:17:32 +0100

    fin de l'interface

--- PartieInterface/main.cpp
+++ PartieInterface/main.cpp
@@ -4,8 +4,56 @@
 #include <ctime>
 #include <iomanip>
 #include <climits>
+#include <sqlite3.h>
 
 using namespace std;
+
+// Fonction qui permet de supprimer un élement de la bdd
+int supprimerElement(const std::string &nomBaseDeDonnees, int id) {
+  sqlite3 *db;
+  int rc;
+  sqlite3_stmt *stmt;
+
+  // Ouvre la base de données
+  rc = sqlite3_open(nomBaseDeDonnees.c_str(), &db);
+  if (rc) {
+    std::cerr << "Erreur lors de l'ouverture de la base de données : " << sqlite3_errmsg(db) << std::endl;
+    return 1;
+  }
+
+  // Prépare la requête de suppression
+  std::string sql = "DELETE FROM ma_table WHERE id=?";
+  rc = sqlite3_prepare_v2(db, sql.c_str(), -1, &stmt, NULL);
+  if (rc) {
+    std::cerr << "Erreur lors de la préparation de la requête : " << sqlite3_errmsg(db) << std::endl;
+    sqlite3_close(db);
+    return 1;
+  }
+
+  // Lie la valeur de l'ID à la requête
+  rc = sqlite3_bind_int(stmt, 1, id);
+  if (rc) {
+    std::cerr << "Erreur lors du liage de la valeur de l'ID : " << sqlite3_errmsg(db) << std::endl;
+    sqlite3_finalize(stmt);
+    sqlite3_close(db);
+    return 1;
+  }
+
+  // Exécute la requête
+  rc = sqlite3_step(stmt);
+  if (rc != SQLITE_DONE) {
+    std::cerr << "Erreur lors de l'exécution de la requête : " << sqlite3_errmsg(db) << std::endl;
+    sqlite3_finalize(stmt);
+    sqlite3_close(db);
+    return 1;
+  }
+
+  // Finalisez et fermez la requête et la connexion à la base de données
+  sqlite3_finalize(stmt);
+  sqlite3_close(db);
+
+  return 0;
+}
 
 // Fonction qui permet de passer en majuscule le premier caractère d'un string
 std::string capitalize(const std::string& str) {
@@ -52,6 +100,7 @@ int main()
     tm date;
     bool bool_date;
     bool bool_sexe;
+    int idSup;
 
     // MENU
     while (!quit)
@@ -136,17 +185,31 @@ int main()
                     }
                 }while(bool_sexe);
 
-                std::cout << "Entrez une entreprise : ";
-                std::getline(std::cin, entreprise);
+                std::cout << "Entrez une entreprise (inserer NULL si pas d entreprise) : ";
+                std::cin >> entreprise;
 
+                std::cout << "Entrez le libelle de rue : ";
+                std::cin >> libelle;
 
+                std::cout << "Entrez un complement d adresse (inserer NULL si pas de complement) : ";
+                std::cin >> complement;
 
+                // A FAIRE : gestion du bon format pour code postal
+                std::cout << "Entrez un code postal : ";
+                std::cin >> codeP;
+
+                std::cout << "Entrez une ville : ";
+                std::cin >> ville;
+                ville = to_upper(ville);
+
+                std::cout << "Entrez un mail (inserer NULL si pas de mail) : ";
+                std::cin >> mail;
 
 
                 // Gestion de la date au bon format
                 do
                 {
-                    std::cout << "Entrez une date au format dd/mm/yyyy : ";
+                    std::cout << "Entrez une date de naissance au format dd/mm/yyyy : ";
                     std::cin >> std::get_time(&date, "%d/%m/%Y"); // utilisation de get_time pour lire la date au format spécifié
                     if (std::cin.fail())
                     {
@@ -161,12 +224,15 @@ int main()
                     }
                 }while(bool_date);
 
-                //std::cout << "Date saisie : " << std::put_time(&date, "%d/%m/%Y") << std::endl;
-
 
 
                 break;
+
+            // Suppression d'un contact via son id
             case 4:
+                std::cout << "Quel est l'identifiant du contact que vous voulez supprimer : ";
+                std::cin >> idSup;
+
 
                 break;
 
