f801c9f3fdb5959880ab4443d0666aebd3ea3ae5
Nicolas Chataignon <nicolas.chataignon@protonmail.com>
2022-12-30 14:34:40 +0100

    main avec multi thread

--- main.cpp
+++ main.cpp
@@ -4,12 +4,110 @@
 
 
 #include "fonctionsDB.h"
+#include "fonctionsCSV.h"
 
+#include <cstdio>
+#include <chrono>
+#include <filesystem>
+#include <iostream>
+#include <thread>
+
+#define CSVPRO "./csv/newprofs.csv"
+#define CSVPRIVE "./csv/newprivates.csv"
+
+bool terminateProgram = false;
 
 int main()
 {
 
 
+
+
+    // interval pour check des fichiers csv.
+    chrono::seconds interval(5);
+    chrono::seconds interval2(1);
+
+    // chemins à surveiller
+    filesystem::path filePro(CSVPRO);
+    filesystem::path filePrive(CSVPRIVE);
+
+    // Thread pour vérif et traitement des CSV
+    thread fileCheckingThread([&] {
+        while (true)
+        {
+
+
+
+            // si fichier existe : import + suppression
+            if (filesystem::exists(filePro))
+            {
+
+                CSVPROtoDB(filePro);
+                cout<<"imported 'newprofs.csv'"<<endl;
+                int result = remove(CSVPRO);
+                if (result != 0) {
+                    cout<<"an error occured during file deletion"<<endl;
+                    return 1;
+                }
+            }
+
+
+            // si fichier existe : import + suppression
+            if (filesystem::exists(filePrive))
+            {
+
+                CSVPRIVEtoDB(filePrive);
+                cout<<"imported 'newprivates.csv'"<<endl;
+                int result = remove(CSVPRIVE);
+                if (result != 0) {
+                    cout<<"an error occured during file deletion"<<endl;
+                    return 1;
+                }
+            }
+
+            // sortir si l'utilisateur a demandé à quitter le programme.
+            if (terminateProgram)
+            {
+                break;
+            }
+
+            // sleep pendant interval
+            this_thread::sleep_for(interval);
+        }
+    });
+
+    // Thread pour l'interface utilisateur
+    thread userInterfaceThread([&] {
+        cout<<"doing something"<<endl
+        ;
+        int i=0;
+        while(1)
+        {
+            i++;
+            cout<<"."<<endl;
+            this_thread::sleep_for(interval2);
+            if (i==4)
+            {
+                terminateProgram=true;
+                break;
+            }
+        }
+    });
+
+
+
+
+    // Wait for the file checking thread to finish
+    fileCheckingThread.join();
+    userInterfaceThread.join();
+    return 0;
+
+
+
+
+
+
+    /*
     // création instances de contacts:
     ContactPro* pro1 = new ContactPro(1,"Wallace","Marcel",'M',"Renault","rue du quai","",78888,"Guyancourt",
                     "marcel.wallace@renault.com");
@@ -21,15 +119,19 @@ int main()
 
 
     cout<<prive1<<endl;
-
+    */
 
     // Ajouter les contacts à la BDD:
-    addContact(pro1);
-    addContact(&prive1);
+//    addContact(pro1);
+//    addContact(&prive1);
 
 
-    delete pro1;
-    return 0;
+
+        //CSVPROtoDB("./csv/newprofs.csv");
+        //CSVPRIVEtoDB("./csv/newprivates.csv");
+
+//    delete pro1;
+
 }
 
 
